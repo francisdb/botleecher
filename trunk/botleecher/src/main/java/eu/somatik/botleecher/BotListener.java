4f5e973773b72e15158e5a2d0ec4206bb399f39c
francisdb <francisdb@9238ae2a-7d21-0410-8510-3d2344257fb1>
2007-02-22 22:59:24 +0000

    code cleanup using the new Netbeans Milestone 7 build total gui rebuild using matisse

--- trunk/botleecher/src/main/java/eu/somatik/botleecher/BotListener.java
+++ trunk/botleecher/src/main/java/eu/somatik/botleecher/BotListener.java
@@ -3,6 +3,15 @@ package eu.somatik.botleecher;
 import org.jibble.pircbot.User;
 
 
+/**
+ * 
+ * @author francisdb
+ */
 public interface BotListener {
+    /**
+     * 
+     * @param channel 
+     * @param users 
+     */
     void userListLoaded(String channel, User[] users);
 }
